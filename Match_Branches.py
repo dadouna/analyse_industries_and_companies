50dfcd87d3869e7a1680e7a123bd14ded5daf4a5
Mona Dadoun <mona.dadoun@gmail.com>
2017-11-29 14:55:54 +0100

    WIP: refactor

--- Match_Branches.py
+++ Match_Branches.py
@@ -4,11 +4,6 @@ import pandas as pd
 #
 
 
-def get_file_path(file_name):
-    file_path = "/Users/carlhasselskog/PycharmProjects/HittaPunktSe/Data/" + file_name
-    return file_path
-
-
 def read_file(source, separator):
 
     df = pd.read_csv(source, sep=separator, header=0)
@@ -16,22 +11,14 @@ def read_file(source, separator):
     return df, col_names
 
 
-def write_file(output_file, label, info, pair1, source, m, kind):
-    filename = output_file + "/" + label + " " + kind + "_out.txt"
-    f = open(filename, 'w+')
-    f.write("Info                 : " + info + "\n")
-    f.write("Source                                    : " + source + "\n")
-    f.write("Classifier                                : " + label + "\n")
-    f.write("Simply the best                           : " + label + " " + str(m) + "\n")
-    f.write("Best TEST Classifier OF ALL is : " + str(pair1) + "\n")
-
-
 def main():
-    branch_path = get_file_path('Branscher.csv')
-    data_frame, column_name = read_file(branch_path, ';')
+    industries_data_file_name = 'Industries.csv'
+    companies_data_file_name = 'Companies.csv'
+    data_frame, column_name = read_file(industries_data_file_name, ';')
     print(data_frame)
-    branch_set = []
     for row in data_frame.iterrows():
-        set(row).add('key')
+        pass
+    
+
 
 main()
